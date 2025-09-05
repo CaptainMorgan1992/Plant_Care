b2be375a07990fc2c7902fc9d678a5386b9e5ade
CaptainMorgan1992 <kimberly.morgan1992@outlook.com>
2025-09-05 14:42:24 +0200

    main is working now

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -21,19 +21,20 @@ public class UserService
         _db = db;
     }
 
-    public async Task<string> GetUserAuth0IdAsync()
+    public async Task<string?> GetUserAuth0IdAsync()
     {
         var authState = await _authStateProvider.GetAuthenticationStateAsync();
         var user = authState.User;
+        
         var userId = user.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
-
-        if (!string.IsNullOrWhiteSpace(userId))
+        
+        if (string.IsNullOrWhiteSpace(userId))
         {
-            return userId;
+            _logger.LogError("UserId is missing. Throwing exception.");
+            return null;
         }
         
-        _logger.LogError("UserId is missing. Throwing exception.");
-        throw new InvalidOperationException("UserId is missing.");
+        return userId;
     }
     
     public async Task<int?> GetUserIdByOwnerIdAsync(string ownerId)
