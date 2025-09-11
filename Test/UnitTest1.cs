283ec33517b33318ae4709b2ee363293d8e9df47
CaptainMorgan1992 <kimberly.morgan1992@outlook.com>
2025-09-11 14:03:13 +0200

    First test has passed

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Auth0_Blazor;
+
+namespace Test;
+
+public class Tests
+{
+    [Test]
+    public void PassingTest()
+    {
+        Assert.That(Calculator.Add(2,2), Is.EqualTo(4));
+    }
+
+    [Test]
+    public void FailingTest()
+    {
+        Assert.That(Calculator.Add(2,2), Is.EqualTo(5));
+    }
+}
