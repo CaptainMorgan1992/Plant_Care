eb0b57e9a3a7800d28797b843e42229db95f3790
CaptainMorgan1992 <kimberly.morgan1992@outlook.com>
2025-09-16 14:33:09 +0200

    PlantService tests done

--- Test/PlantServiceTest.cs
+++ Test/PlantServiceTest.cs
@@ -3,6 +3,7 @@ using Auth0_Blazor.Enums;
 using Auth0_Blazor.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Moq;
 
 namespace Test;
@@ -15,7 +16,6 @@ public class PlantServiceTest
     private Mock<ILogger<UserService>> _loggerMock = null!;
     private Mock<ApplicationDbContext> _dbMock = null!;
     private UserService _userService = null!;
-    
     
     [SetUp]
     public void Setup()
@@ -59,4 +59,87 @@ public class PlantServiceTest
         var result = await plantService.GetAllPlantsAsync();
         Assert.That(result.Count, Is.EqualTo(2));
     }
+
+    [Test]
+    public async Task GetOnePlantById_ShouldReturnPlantObject_OrReturnExceptionIfNotFound()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) 
+            .Options;
+
+        var dbContext = new ApplicationDbContext(options);
+        
+        dbContext.Plants.Add(new Auth0_Blazor.Models.Plant
+        {   Id = 1,
+            Name = "Plant1", 
+            Description = "Some description",
+            ImageUrl = "https://example.com/plant1.jpg",
+            Origin = "Origin1",
+            WaterFrequency = WaterFrequency.Normal
+        });
+        
+        await dbContext.SaveChangesAsync();
+        
+        var plantService = new PlantService(dbContext, _userService);
+        var result = await plantService.GetPlantByIdAsync(1);
+        
+        Assert.That(result.Name, Is.EqualTo("Plant1"));
+        Assert.That(result.Description, Is.EqualTo("Some description"));
+        Assert.That(result.ImageUrl, Is.EqualTo("https://example.com/plant1.jpg"));
+        Assert.That(result.Origin, Is.EqualTo("Origin1"));
+        Assert.That(result.WaterFrequency, Is.EqualTo(WaterFrequency.Normal));
+        
+        Assert.ThrowsAsync<KeyNotFoundException>(async () => await plantService.GetPlantByIdAsync(999));
+    }
+
+    [Test]
+    public async Task AddNewPlant_ShouldThrowException_IfUserIsNotAdmin()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        var dbContext = new ApplicationDbContext(options);
+        var userService = new UserService(_authStateProviderMock.Object, _loggerMock.Object, dbContext);
+        var plantService = new PlantService(dbContext, userService);
+        
+        var newPlant = new Auth0_Blazor.Models.Plant
+        {
+            Name = "Plant1",
+            Description = "Some description",
+            ImageUrl = "https://example.com/plant1.jpg",
+            Origin = "Origin1",
+            WaterFrequency = WaterFrequency.Normal
+        };
+        
+        var nonAdminUser = new Auth0_Blazor.Models.User
+        {
+            OwnerId = "non-admin-owner-id",
+            IsAdmin = false,
+            Name = "NonAdminUser",
+        };
+        
+        var adminUser = new Auth0_Blazor.Models.User
+        {
+            OwnerId = "admin-owner-id",
+            IsAdmin = true,
+            Name = "AdminUser"
+        };
+
+        dbContext.Users.Add(nonAdminUser);
+        dbContext.Users.Add(adminUser);
+        
+        await dbContext.SaveChangesAsync();
+        
+        var ownerIsNotAdmin = await userService.IsUserAdminAsync("non-admin-owner-id");
+        Assert.That(ownerIsNotAdmin, Is.False);
+        Assert.ThrowsAsync<UnauthorizedAccessException>(async () =>
+            await plantService.AddNewPlantAsync(newPlant, "non-admin-owner-id"));
+        
+        var ownerIsAdmin = await userService.IsUserAdminAsync("admin-owner-id");
+        Assert.That(ownerIsAdmin, Is.True);
+        var result = await plantService.AddNewPlantAsync(newPlant, "admin-owner-id");
+        Assert.That(result, Is.True);
+        
+    }
+
 }
